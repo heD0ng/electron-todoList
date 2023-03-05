2baa5ac5d65af304f9ec8d6dbdbafda8947fc9ce
hedong <13545403924@163.com>
2023-03-05 15:58:32 +0800

    feat:修改崩溃日志路径

--- src/main/utils/crash.ts
+++ src/main/utils/crash.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { app, crashReporter } from 'electron';
-
-const crashPath = path.resolve(process.cwd(), './release/crashes')
+const appPath = app.getAppPath();
+const crashPath = path.resolve(appPath, 'crashes')
 app.setPath('crashDumps', crashPath);
 
 // 该方法应尽早地在app启动后调用，最好在 app.on('ready') 之前。
